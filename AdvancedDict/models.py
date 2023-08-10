7b9a8af3f37da03d5e8d750e71698ec36963ece9
Danylo <danylo.tovpyshko@gmail.com>
2023-08-10 13:45:30 +0200

    Added languages codes file, created entry model

--- AdvancedDict/models.py
+++ AdvancedDict/models.py
@@ -3,6 +3,17 @@ from django.contrib.auth.models import AbstractUser
 
 from datetime import datetime
 # Create your models here.
+# from language_codes import languages
 
 class User(AbstractUser):
     pass
+
+class entry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.s("New Word", max_length=50)
+    source_language = models.CharField("Source language code",max_length=5)
+    target_language = models.CharField("Target language code",max_length=5)
+    translation = models.CharField("Translation", max_length=50)
+    
+
+    
